1f96cf54d063d77c441da4bec1a94481ecf4cb95
achados & bompreço <254976205+achado1321@users.noreply.github.com>
2026-01-22 14:01:09 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-// 🔥 CONFIG FIREBASE (cole o seu aqui)
+// 🔥 CONFIG FIREBASE)
 const firebaseConfig = {
   apiKey: "AIzaSyB2hYymrzOG__95wxyrG3soEjinVD9ONvM",
   authDomain: "achadosebompre.firebaseapp.com",

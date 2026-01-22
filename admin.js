08d397cb0b02548d0907a0a6a7d48bf1841386d7
achados & bompreço <254976205+achado1321@users.noreply.github.com>
2026-01-22 14:00:53 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,16 @@
+// 🔥 CONFIG FIREBASE (cole o seu aqui)
+const firebaseConfig = {
+  apiKey: "AIzaSyB2hYymrzOG__95wxyrG3soEjinVD9ONvM",
+  authDomain: "achadosebompre.firebaseapp.com",
+  projectId: "achadosebompre",
+  storageBucket: "achadosebompre.firebasestorage.app",
+  messagingSenderId: "885306134293",
+  appId: "1:885306134293:web:a167546fe9c8a7662e231c"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
 
